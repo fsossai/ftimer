28f0e7baa9af3be322f42720c0efd50a6dead66d
fsossai <federico.sossai@gmail.com>
2023-12-06 19:52:53 -0600

    Added custom time formats

--- horatio/__init__.py
+++ horatio/__init__.py
@@ -1,18 +1,47 @@
 import time
+import datetime
 import fslog
 
-unit = "s"
+fmt = None
 no_desc_default = "\033[0m\u2510"
 
-def format(t):
-    scale = {"s":1, "ms":1000, "us":1000000, "m":1/60, "h":1/3600}
-    t *= scale[unit]
-    return f"{t:.3f} {unit}"
+def format_time(t, fmt=None):
+    gfmt = globals()["fmt"]
+    fmt = fmt or gfmt
+    t = 0*86400 + 0*11*3600 + 0*12*60 + 1.12
+    d = dict()
+    d["f"] = int(t*1000) / 1000
+    d["ms"] = int(t * 1000)
+    d["s"] = int(t)
+    d["m"] = int(t / 60)
+    d["h"] = int(t / 3600)
+    d["S"] = int(t) % 60
+    d["M"] = int(int(t) % 3600 / 60)
+    d["H"] = int(int(t) % 86400 / 3600)
+    d["D"] = int(t / 86400)
+
+    if fmt is not None:
+        return fmt.format(**d)
+    
+    if t >= 86400: # days
+        fmt  = "{D}d {H:02}h {M:02}m {S:02}s"
+    elif t >= 3600: # hours
+        fmt  = "{h}h {M:02}m {S:02}s"
+    elif t >= 60: # minutes
+        fmt  = "{m}m {S:02}s"
+    elif t >= 1: # seconds
+        fmt  = "{f}s"
+    elif t >= 0.001: # milliseconds
+        fmt = "{ms}ms"
+    else: # too small
+        fmt = "< 1 ms"
+    return fmt.format(**d)
 
 class step():
-    def __init__(self, desc=None):
+    def __init__(self, desc=None, fmt=None):
         self.ts = []
         self.desc = desc
+        self.fmt = fmt
 
     def __enter__(self):
         if self.desc is None:
@@ -24,7 +53,7 @@ class step():
     def __exit__(self, *args):
         t = self.ts.pop()
         t = time.time() - t
-        fslog.plain("done in {}\n".format(format(t)), end="")
+        fslog.plain("done in {}\n".format(format_time(t, self.fmt)), end="")
 
     def __call__(self, f):
         if self.desc is None:
@@ -37,10 +66,11 @@ class step():
         return wrapper
 
 class flat():
-    def __init__(self, desc=None, tail=None):
+    def __init__(self, desc=None, tail=None, fmt=None):
         self.ts = []
         self.desc = desc
         self.tail = tail
+        self.fmt = fmt
 
     def __enter__(self):
         if self.desc is None:
@@ -53,7 +83,7 @@ class flat():
     def __exit__(self, *args):
         t = self.ts.pop()
         t = time.time() - t
-        tf = format(t)
+        tf = format_time(t, self.fmt)
         if self.tail is None:
             if self.desc is None:
                 fslog.log("[*] {}".format(tf))
@@ -73,10 +103,11 @@ class flat():
         return wrapper
 
 class section():
-    def __init__(self, desc=None, tail=None):
+    def __init__(self, desc=None, tail=None, fmt=None):
         self.ts = []
         self.desc = desc
         self.tail = tail
+        self.fmt = fmt
 
     def __enter__(self):
         if self.desc is None:
@@ -88,7 +119,7 @@ class section():
     def __exit__(self, *args):
         t = self.ts.pop()
         t = time.time() - t
-        tf = format(t)
+        tf = format_time(t, self.fmt)
         if self.tail is None:
             if self.desc is None:
                 fslog.close("{}".format(tf))
